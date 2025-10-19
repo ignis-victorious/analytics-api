cc8558339e66aea9d54282339d4d05127d40ec6d
Emmanuele Levi <ignisvictorious@gmail.com>
2025-10-19 21:25:18 +0100

    Some bugs

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -10,23 +10,33 @@ from .schemas import EventListSchema, EventSchema
 router: APIRouter = APIRouter()
 
 
-# /api/events/
+# /api/events/ß
 @router.get(path="/")
 def read_events() -> EventListSchema:
     # a bunch of items in a
     return {"results": [{"id": 1}, {"id": 2}, {"id": 3}], "count": 3}
 
 
-# L to chat, &K to generate
-
-# @router.get(path="/")
-# def read_events() -> dict[str, list[int]]:
-#     # a bunch of items in a
-#     return {"results": [1, 2, 3]}
+# create view
+# POST(SEND DATA) /api/events/
+@router.post(path="/")
+def create_event(data: dict = {}) -> EventSchema:
+    # a bunch of items in a table
+    print("_________")
+    print(type(data))
+    return {"id": 123}
 
 
 @router.get(path="/{event_id}")
 def get_event(event_id: int) -> EventSchema:
+    # a single row
+    return {"id": event_id}
+
+
+# Update this data
+# PUT /api/events/12
+@router.put(path="/{event_id}")
+def update_event(event_id: int, payload: dict = {}) -> EventSchema:
     # a single row
     return {"id": event_id}
 
